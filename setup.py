55177c2a685e3eba39606dac5287058e6fcd6485
fastily <fastily@users.noreply.github.com>
2024-08-26 01:58:20 -0700

    tag for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="four-chan-ripper",
-    version="0.0.1",
+    version="0.0.2",
     author="Fastily",
     author_email="fastily@users.noreply.github.com",
     description="Rips the contents of threads on 4chan",
